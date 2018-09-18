tmc/notion-prev

client: initial commit

client_test.go
@@ -1,0 +1,1 @@
+package notion

errors.go
@@ -1,0 +1,14 @@
+package notion
+
+import "fmt"
+
+// Error represents an error returned from the notion.so API.
+type Error struct {
+	URL        string
+	StatusCode int
+	Body       string
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("notion: %v %v '%.100s'", e.StatusCode, e.URL, e.Body)
+}

tmc/circleci

client_test.go
@@ -1,0 +1,1 @@
+package circleci

errors.go
@@ -1,0 +1,14 @@
+package circleci
+
+import "fmt"
+
+// Error represents an error returned from the circleci.com API.
+type Error struct {
+	URL        string
+	StatusCode int
+	Body       string
+}
+
+func (e *Error) Error() string {
+	return fmt.Sprintf("circleci: %v %v '%.100s'", e.StatusCode, e.URL, e.Body)
+}